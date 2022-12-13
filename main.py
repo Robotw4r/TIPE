0e7fbeecd0a4cd2a8453963627c3aee5ac712042
Rob <astyanax.destroyes@gmail.com>
2022-12-13 13:19:43 +0100

    First version of mode switch (not working yet and disabled)

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from stockfish import Stockfish
 from tkinter import *
-import time
 
 stockfish = Stockfish(path="/usr/games/stockfish")
+
+autoMode = True
 
 def updateBoard():
     move = [stockfish.get_best_move()]
     stockfish.make_moves_from_current_position(move)
     return stockfish.get_board_visual()
+'''
+def updateManual():
+    playermove = [pièce]
+    stockfish.make_moves_from_current_position(playermove)
+    move = [stockfish.get_best_move()]
+    stockfish.make_moves_from_current_position(move)
+    return stockfish.get_board_visual()
+'''
 
 def nextTurn():
-    my_label.config(text = updateBoard())
+    if autoMode == True:
+        my_label.config(text = updateBoard())
+    #else:
+    #    my_label.config(text = updateManual())
 
 Main_window = Tk()
 
@@ -21,8 +33,13 @@ my_button = Button(Main_window,
                    text="Next turn",
                    command=nextTurn)
 
+#boutonMode = Button(Main_window,text="Change Mode (Auto / Manual)", command = print("LOL") )
+
+#pièce = Entry(Main_window)
 
 my_label.pack()
 my_button.pack()
+#boutonMode.pack()
+#pièce.pack()
 
 Main_window.mainloop()
